09e9699aba91f65272e2c415961418ab3187b056
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2025-03-18 07:34:54 -0300

    feat: adds configuration

--- src/config/config.go
+++ src/config/config.go
@@ -1,1 +1,32 @@
 package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	StrinConexaoBanco = ""
+	Porta             = 0
+)
+
+func Carregar() {
+	var erro error
+
+	if erro = godotenv.Load(); erro != nil {
+		log.Fatal("Erro ao carregar .env:", erro)
+	}
+
+	Porta, erro = strconv.Atoi(os.Getenv("API_PORT"))
+	if erro != nil {
+		Porta = 9000
+	}
+
+	StrinConexaoBanco = fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("DB_USUARIO"), os.Getenv("DB_SENHA"), os.Getenv("DB_NOME"))
+
+}
