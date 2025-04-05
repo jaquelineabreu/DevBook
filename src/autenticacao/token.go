580eb0a451b2b6eaa4d3d933ae8a4fc0035b8b6b
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2025-04-05 19:22:19 -0300

    feat: validação de token

--- src/autenticacao/token.go
+++ src/autenticacao/token.go
@@ -2,6 +2,9 @@ package autenticacao
 
 import (
 	"api/src/config"
+	"fmt"
+	"net/http"
+	"strings"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
@@ -15,4 +18,35 @@ func CriarToken(usuarioId uint64) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, permissoes)
 	return token.SignedString([]byte(config.SecretKey))
 
+}
+
+func ValidarToken(r *http.Request) error {
+	tokenString := extrairToken(r)
+
+	token, erro := jwt.Parse(tokenString, retornarChaveVeficacao)
+	if erro != nil {
+		return erro
+	}
+
+	fmt.Println(token)
+
+	return nil
+}
+
+func extrairToken(r *http.Request) string {
+	token := r.Header.Get("Authorization")
+
+	if len(strings.Split(token, " ")) == 2 {
+		return strings.Split(token, " ")[1]
+	}
+
+	return ""
+}
+
+func retornarChaveVeficacao(token *jwt.Token) (interface{}, error) {
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return nil, fmt.Errorf("Método de assinaturan inesperado! %v", token.Header["alg"])
+	}
+
+	return config.SecretKey, nil
 }
